ed2801c540675e649b210ae4e1424b6b5ea00ab4
Pavel <grebeniuk.p@gmail.com>
2019-12-29 20:48:21 +0200

    init commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+//Get ATM PrivatBank
+const cl = arg=>console.log(arg);
+let devicesList;
+let atmLocale =[];
+let obj = {};
+
+const data = fetch('https://pahich09.github.io/lesson08/data.json');
+	data
+		.then(responce=> responce.json())
+		.then(responce=>{
+			devicesList = responce.devices;
+			getUnicAtmLocal(devicesList);
+			getAtmList(atmLocale, devicesList);
+		cl(obj);
+		})
+		.catch(error=>console.log('error', error))
+
+function getUnicAtmLocal(array) {
+	array.forEach((el, i)=>{
+		if(!atmLocale.includes(array[i].placeRu)){
+			atmLocale.push(array[i].placeRu);
+		}
+	})
+}
+function getAtmList(arr, array) {
+	arr.forEach((item)=>{
+	obj[item] = [];
+		array.forEach((elem, index)=>{
+			if(item == array[index].placeRu){
+				obj[item].push(array[index]);
+			}
+		})	
+	})
+}
